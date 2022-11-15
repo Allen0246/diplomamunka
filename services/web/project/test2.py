32ec83ff73946828bd8131fc1e6605459c1b1706
Allen0246 <kelemen.rihcard1@gmail.com>
2022-11-15 10:20:39 +0100

    .

--- services/web/project/test2.py
+++ services/web/project/test2.py
@@ -7,6 +7,8 @@ import requests
 import urllib3
 import json
 import pandas as pd
+from project import movie_request,url_movie,params_movie
+from .model.movie import Movie
 from . import db
 
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
@@ -21,17 +23,17 @@ params_api = {
 }
 
 
-
-def genre_request(url_genre, params_api ):
-    response = requests.get(url_genre, headers="", params=params_api, verify=False)
-    if  response.status_code == 200:
-        response = json.loads(response.text, strict=False)
-        return response
-    else:
-        response = json.loads(response.text, strict=False)
-        return response["status_message"]
-
-data = genre_request(url_genre, params_api )
-print(data)
-
+movie_data = movie_request(url_movie, params_movie)
+if type(movie_data) != str:
+    for r in movie_data['results']:
+        print (r)
+        # movie_db = Movie.query.filter_by(movie_data['title']).first()
+        # if not movie_db:
+        #     for x in range (1,get_pages(movie_data)+1):
+        #         params_movie['page']=x
+        #         title_list = (title_result1(url_movie, params_movie))
+        #         genre_id = (genre_result1(url_movie, params_movie))
+        #         movie_result = Movie(title_list,genre_id)
+        #         db.session.add(movie_result)
+        #         db.session.commit()
 
